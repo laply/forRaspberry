b2a0dd836f6164aaec597d019a8be7d8380d3230
laply <lapply2635@gmail.com>
2020-03-25 17:04:31 +0900

    test senser1.py

--- test/sensing.py
+++ test/sensing.py
@@ -10,6 +10,10 @@ client.connect("127.30.1.14")
 topic_temp = "tcs/dht11/temp"
 topic_humid = "tcs/dht11/humid"
 
+# pin setting 
+fire_pin = 25
+led_red = 23
+led_green = 24
 
 # initialize GPIO
 GPIO.setwarnings(False)
@@ -18,27 +22,50 @@ GPIO.cleanup()
 
 # read data using pin
 instance = dht11.DHT11(pin = 22)
+GPIO.setup(fire_pin, GPIO.IN)
+GPIO.setup(led_green, GPIO.OUT)
+GPIO.setup(led_red, GPIO.OUT)
+
+# etc 
+tempHumidCount = 0 
 
 def tempHumid():
 	result = instance.read()
 	if result.is_valid():
+		tempHumidCount++
 		now_time = "Last valid input: " + str(datetime.datetime.now())
 		temp = "Temperature: %d C" % result.temperature
 		humid = "Humidity: %d %%" % result.humidity
 
-		client.publish(topic_temp, temp)
-		client.publish(topic_humid, humid)
+		if(tempHumidCount == 10)
+			client.publish(topic_temp, temp)
+			client.publish(topic_humid, humid)
+			tempHumidCount = 0
 
 		print(now_time)
 		print(temp)
 		print(humid)
 
-		time.sleep(10)
+		#gpio.output(trig_pin, False)
+		#while gpio.input(echo_pin) == 0:
+		#	pulse_start = time.time()
+
+def firedata():
+	if GPIO.input(fire_pin) == 1:
+		GPIO.output(led_red, True)
+		GPIO.output(led_green, False)
+	else :
+		GPIO.output(led_green, True)
+		GPIO.output(led_red, False)
 
 try:
 	client.loop_start()
 	while True:
 		tempHumid()
+		firedata()
+
+
+
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
