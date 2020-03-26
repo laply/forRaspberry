ebb9d10ca16d0b55f3af6526eb2e7ca16a8f38ba
laply <lapply2635@gmail.com>
2020-03-26 10:27:30 +0900

    ls

--- test/IRSensor.py
+++ test/IRSensor.py
@@ -1,21 +1,18 @@
 import RPi.GPIO as GPIO
 import time 
 
-s_led_red_pin = 21
 s_led_green_pin = 20
 ir_sensor_pin = 16
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(s_led_red_pin, GPIO.OUT)
 GPIO.setup(s_led_green_pin, GPIO.OUT)
 GPIO.setup(ir_sensor_pin, GPIO.IN)
 
 try :
     while True :
-        if GPIO.input(ir_sensor_pin) == 0:
-	    print("out")
-            GPIO.output(s_led_red_pin, True)
-
+        if GPIO.input(ir_sensor_pin) == 1:
+    	    print("out")
+            GPIO.output(s_led_green_pin, True)
 
 
 except KeyboardInterrupt :
