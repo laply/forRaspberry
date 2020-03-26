5c378598f3871e6cafd923e2f6600f2d34213fa8
laply <lapply2635@gmail.com>
2020-03-26 09:59:14 +0900

    ls

--- test/IRSensor.py
+++ test/IRSensor.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time 
 
-s_led_red_pin = 20
-s_led_green_pin = 21
+s_led_red_pin = 21
+s_led_green_pin = 20
 ir_sensor_pin = 16
 
 GPIO.setmode(GPIO.BCM)
@@ -16,7 +16,6 @@ try :
             GPIO.output(s_led_green_pin, True)
             GPIO.output(s_led_red_pin, False)
         else :
-            print("on")
             GPIO.output(s_led_green_pin, False)
             GPIO.output(s_led_red_pin, True)
 	    
