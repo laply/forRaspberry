58cf0e31cb011dc7b3aa78fd907adaad214459b0
laply <lapply2635@gmail.com>
2020-03-27 16:47:25 +0900

    ls

--- test/sensing_test.py
+++ test/sensing_test.py
@@ -12,12 +12,12 @@ topic_humid = "tcs/humid"
 topic_fire = "tcs/fire"
 topic_shock = "tcs/shock"
 topic_IR = "tcs/ir"
-topic_clear = "tcs/clear"
+topic_Clear = "tcs/clear"
 
 # pin setting
 fire_pin = 25
-led_red_pin = 24
-led_green_pin = 23
+led_red = 23
+led_green = 24
 shock_pin = 27
 ir_sensor_pin = 16
 button_pin = 26
@@ -30,8 +30,8 @@ GPIO.cleanup()
 # read data using pin
 instance = dht11.DHT11(pin = 22)
 GPIO.setup(fire_pin, GPIO.IN)
-GPIO.setup(led_green_pin, GPIO.OUT)
-GPIO.setup(led_red_pin, GPIO.OUT)
+GPIO.setup(led_green, GPIO.OUT)
+GPIO.setup(led_red, GPIO.OUT)
 GPIO.setup(shock_pin, GPIO.IN)
 GPIO.setup(ir_sensor_pin, GPIO.IN)
 GPIO.setup(button_pin, GPIO.IN)
@@ -40,7 +40,7 @@ GPIO.setup(button_pin, GPIO.IN)
 tHCount = 0
 fireFlag = 0
 shockFlag = 0
-IRFlag = 0
+IRFlag = 0 
 
 def tempHumid():
 	result = instance.read()
@@ -56,49 +56,54 @@ def tempHumid():
 			client.publish(topic_humid, humid)
 			tHCount = 0
 
-			print(now_time)
-			print(temp)
-			print(humid)
+		print(now_time)
+		print(temp)
+		print(humid)
 
 		tHCount += 1
 
-def led(i : int):
-	if(i == 0):
-		GPIO.output(led_green_pin, False)
-		GPIO.output(led_red_pin, True)
-	elif(i == 1)
-		GPIO.output(led_green_pin, True)
-		GPIO.output(led_red_pin, False)
-
 def fireData():
 	if GPIO.input(fire_pin) == 1:
-		led(0)
+		GPIO.output(led_red, True)
+		GPIO.output(led_green, False)
 		client.publish(topic_fire, 1)
+#	else :
+#		global fireFlag
+#		if(fireFlag == 3000):
+#			client.publish(topic_fire, 0)
+#			fireFlag = 0
+
+#		fireFlag += 1
 
 def shockData():
-	if GPIO.input(shock_pin) == 1:
-		print("shock")
-		led(0)
+    if gpio.input(shock_pin) == 1:
+        print("shock")
+        gpio.output(led_green_pin, False)
+        gpio.output(led_red_pin, True)
 		client.publish(topic_shock, 1)
+#    else :
+#		global shockFlag
+#		if(shockFlag == 3000):
+#			client.publish(topic_shock, 0)
+#			shockFlag = 0
+
+#		shockFlag += 1
 
 def IRData():
 	if GPIO.input(ir_sensor_pin) == 0 :
-		print("detect")
-		led(0)
-		client.publish(topic_IR, 1)
-
+		Print("detect")
+        gpio.output(led_green_pin, False)
+        gpio.output(led_red_pin, True)
+		Client.publish(topic_IR, 1)
 
 def clearButton():
-	if GPIO.input(button_pin) == 1 :
-		client.publish(topic_fire, 0)
-		client.publish(topic_shock, 0)
-		client.publish(topic_IR, 0)
-		client.publish(topic_clear, 1)
-		led(1)
+	if GPIO.input(button_pin) == 1:
+	    gpio.output(led_green_pin, True)
+        gpio.output(led_red_pin, False)
+		client.publish(topic_Clear, 1)
 
 
 try:
-	led(1)
 	client.loop_start()
 	while True:
 		tempHumid()
