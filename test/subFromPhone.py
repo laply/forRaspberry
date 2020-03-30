b51f232f1ee6529f6eb667eb77aba679afab9679
laply-ras <lapply2635@gmail.com>
2020-03-30 07:12:23 +0100

    don't

--- test/subFromPhone.py
+++ test/subFromPhone.py
@@ -1,26 +1,28 @@
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, rc):
-    print("connected with result code " + str(rc))
-    client.subscribe("tcs/phone")
+	print("connected with result code " + str(rc))
+	client.subscribe("tcs/phone")
 
-        
+
 def on_message(client, userdata, msg):
-    print("Topic: " + msg.topic + " Message: " + str(msg.payload))
+	print("Topic: " + msg.topic + " Message: " + str(msg.payload))
 
 client = mqtt.Client()
+
 client.on_connect = on_connect
 client.on_message = on_message
 
 # YOU NEED TO CHANGE THE IP ADDRESS OR HOST NAME
 # client.connect("192.168.0.23", 1883, 60)
-client.connect("172.30.1.14", 1883, 60)
+client.connect("localhost")
 
 #client.connect("localhost")
 
 try:
-    client.loop_forever()
+	print("start")
+	client.loop_forever()
 except KeyboardInterrupt:
-    print("Finished!")
-    client.unsubscribe(["tcs/phone"])
-    client.disconnect()
+	print("Finished!")
+	client.unsubscribe(["tcs/phone"])
+	client.disconnect()
