02f644b60c7bac0f233aae1931ebc8e79a496f95
laply-ras <lapply2635@gmail.com>
2020-03-31 05:13:47 +0100

    ok

--- mqttBrokertest/test0331.py
+++ mqttBrokertest/test0331.py
@@ -1,30 +1,31 @@
 import paho.mqtt.client as mqtt
+import time
 
 client = mqtt.Client()
 
 def on_connect(client, userdata, rc):
-    print("connected result code " + str(rc))
-    client.subscribe("tcs/test/phone")
+	print("connected result code " + str(rc))
+	client.subscribe("tcs/test/phone")
 
 def on_message(client, userdata, msg):
-    print("Topic: " + msg.topic + " Message: " + str(msg.payload))
+	print("Topic: " + msg.topic + " Message: " + str(msg.payload))
+
+
+client.on_connect = on_connect
+client.on_message = on_message
 
 client.connect("115.20.144.97", 11183, 60)
 
-print("start_to_send")
-client.loop_start()
-client.publish("tcs/test1", "hello")
-client.loop_stop()
-client.disconnect()
-print("end_to_send")
-
-
 
 try:
-    client.loop_start()
-    print("start")
-    while True:
-        client.publish("tcs/", "hello")
+	client.loop_start()
+	print("start")
+	client.publish("tcs/test/broker","hello")
+
+    	while True:
+		time.sleep(1)
+		print("...")
+		client.publish("tcs/test/broker", "hi")
 
 except KeyboardInterrupt:
     client.unsubscribe(["tcs/test/phone"])
