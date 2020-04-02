517f68c89a8eccd2059405a5601ea72f9e01ad92
Laply <lapply2635@gmail.com>
2020-04-02 16:08:59 +0900

    update_0402

--- sensingData4/sensingMain5.py
+++ sensingData4/sensingMain5.py
@@ -8,24 +8,28 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
+# localIp = os.popen('hostname -I').read() ##  local ip
+#local port "8891"
+
 localGlobalIp = os.popen('curl ifconfig.me').read() ## global ip
 cameraPort = "11092"
-# localIp = os.popen('hostname -I').read() ##  local ip
+
+cameraIpPort = [ localGlobalIp , cameraPort ]
+
 # dev server IP 
 # brokerIppPort = ["124.139.136.86", "1883"]  
+
 # test server IP
 brokerIpPort = ["115.20.144.97", "11183"]
 
-#local port "8891"
 
 cameraCheck = os.system('sh /home/pi/raspstart/mjpg.sh &') ##
 
-cameraIpPort = [ localGlobalIp , cameraPort ]
 # Can Use GPIO
-# mainInstance = sensor.Sensor(GPIO, localGlobalIp)
+mainInstance = sensor.Sensor(GPIO, brokerIpPort,cameraIpPort)
 
 # Can not use GPIO
-mainInstance = testNoPin.Sensor(GPIO, brokerIpPort, cameraIpPort)
+# mainInstance = testNoPin.Sensor(GPIO, brokerIpPort, cameraIpPort)
 
 def startToSensing():
 		mainInstance.sensingStart()
