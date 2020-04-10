2dabe9382a3d1a72978cc77ff2b4e498b4df37fc
Laply <lapply2635@gmail.com>
2020-04-10 18:45:23 +0900

    all new update0410

--- sensingData/Cds.py
+++ sensingData/Cds.py
@@ -1,8 +1,5 @@
 import datetime
-# detect
-
-def cdsCheck(self):
-	print("cds")	
+# timer
 
 class Cds:
 	def __init__(self, pin, GPIO):
@@ -14,4 +11,29 @@ class Cds:
 		self.GPIO.setup(self.__pin, self.GPIO.IN)
 
 	def read(self):
-		return self.GPIO.input(self.__pin)
+    		
+class Control:
+	def __init__(self, pin, GPIO, topic, topicNum):
+    	self.cds_instance = Cds(pin, GPIO)
+		self.topic = topic
+		self.topicNum = topicNum
+
+		self.detectCheckLastTime = ""
+		self.lastdata = self.false
+
+	def check(self):
+		read = self.cds_instance.read()
+		if read == self.false and self.lastdata == self.false :  
+			self.lastdata = self.true
+			self.topic.setSendMessageTopic(0, self.topicNum, self.lastdata)
+
+			print("MQTT-send - " + "cds")
+		elif read == self.false and self.lastdata == self.false :
+    		self.lastdata = self.false
+			self.topic.setSendMessageTopic(0, self.topicNum, self.lastdata)
+
+			print("MQTT-send - " + "cds")
+
+
+	def getNowData(self):
+			self.topic.setSendMessageTopic(0, self.topicNum, self.lastdata)
