30886750b70ab9887b4f731574b44266466d278b
Laply <lapply2635@gmail.com>
2020-04-10 15:12:46 +0900

    all new update0410

--- sensingData/Connect.py
+++ sensingData/Connect.py
@@ -7,9 +7,6 @@ class Connect :
 
     def __init__(self, ipPort):
         print("MQTT-init")
-        self.connect(ipPort)
-
-	def connect(self, ipPort):
 		self.client.connect(ipPort[0], ipPort[1])
         print("MQTT-connect")
         try:
