4b83680de9d2193492b7b8dc6a786c13df5d09b3
Laply_rasp_test1 <munu2635@gmail.com>
2020-04-17 08:57:17 +0100

    update0417

--- testcode/sensortest/A/mcp3208.py
+++ testcode/sensortest/A/mcp3208.py
@@ -10,9 +10,15 @@ def analog_read(channel):
     adc_out = ((r[1]&3) << 8) + r[2]
     return adc_out
 
-while True:
-    reading = analog_read(0)
-    voltage = reading * 3.3 / 1024
+try :
+    while True:
 
-    print("Reading=%d\tVoltage=%f" % (reading, voltage))
-    time.sleep(1)
+        reading0 = analog_read(0)
+        voltage0 = reading0 * 3.3 / 1024
+
+        print("Reading1=%d - Voltage=%f" % (reading0, voltage0))
+
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    spi.close()
