cd5a26ca40c6da1002827345bbc8aba128970cb2
Laply_rasp_test1 <munu2635@gmail.com>
2020-04-20 05:20:02 +0100

    update

--- testcode/sensortest/A/mcp3208.py
+++ testcode/sensortest/A/mcp3208.py
@@ -4,6 +4,7 @@ import time
 spi = spidev.SpiDev()
 
 spi.open(0, 1)
+spi.max_speed_hz = 1000000
 
 def analog_read(channel):
     r = spi.xfer2([1, (8 + channel) << 4, 0])
@@ -13,12 +14,9 @@ def analog_read(channel):
 try :
     while True:
         num = 0
-        reading = [analog_read(0), analog_read(1)]
-        voltage = [reading[0] * 3.3 / 1024, reading[1] * 3.3 / 1024]
-        
-        for i in reading :
-            print("Reading %d = %d - Voltage = %f" % (num , reading[num], voltage[num]))
-            num = num + 1
+        reading = analog_read(num)
+        voltage = reading * 3.3 / 1024
+	print("Reading = %d - Voltage = %f" % (reading, voltage))
 
         print("")
         time.sleep(1)
