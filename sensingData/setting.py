635db4a1acff340e9ca788cefb560a66e1924211
Laply <lapply2635@gmail.com>
2020-04-20 12:00:53 +0900

    update0420

--- sensingData/setting.py
+++ sensingData/setting.py
@@ -1,7 +1,3 @@
-# cds 잘가는지 확인 
-# gas sensor 및 먼지센서 처리
-# 모두 import 하려면?
-
 raspid = "test1"
 
 brokerIpPort = ["124.139.136.86", "1883"]
@@ -14,11 +10,11 @@ useSensor = [True, True, True, True, False, False, True, True, True ]
 
 # ADC 사용 [7, 9, 10, 11]
 adc_pin = [3]
-	# gas_adc_pin = 3 
+	# gas_adc_pin = 3
 
 all_pin = [22, 25, 24, 23, 27, 16, 26, 5, 20, 21]
 	# dht11_pin = 22 / fire_pin = 25 / led_red_pin = 24 / led_green_pin = 23
-	# shock_pin = 27 / ir_sensor_pin = 16 / button_pin = 26 
+	# shock_pin = 27 / ir_sensor_pin = 16 / button_pin = 26
 	# cds_pin = 5 / motor_pin_1 = 20 / motor_pin_2 = 21
 
 sensordata = [all_pin, adc_pin, useSensor]
@@ -27,5 +23,5 @@ sensordata = [all_pin, adc_pin, useSensor]
 # brokerIpPort = ["124.139.136.86", "1883"]
 
 # test server IP
-#brokerIpPort = ["115.20.144.97", "11183"]ttt
+#brokerIpPort = ["115.20.144.97", "11183"]
 
