584fc75768301f49e49a15b5cb7c74dbfe835ab3
Laply <lapply2635@gmail.com>
2020-04-21 17:35:22 +0900

    update0421

--- testcode/sensortest/A/pm2008test1.py
+++ testcode/sensortest/A/pm2008test1.py
@@ -5,15 +5,11 @@ i2c = smbus.SMBus(1)
 loc = 0x28
 TxBuffer = [0x11, 0x02, 0x0B, 0x07, 0xDB] 
 
-for TxCnt in TxBuffer :
-    i2c.write_byte(loc, TxCnt)
-    time.sleep(0.0005)    
-    
-value_buffer = []
-for i in range(0, 32) :
-    read = i2c.read_byte(0x28)
-    print (read)
-    value_buffer.append(read)
+i2c.write_i2c_block_data(loc, 0x16, TxBuffer)  
+
+value_buffer = i2c.read_i2c_block_data(0x28, 0x16)
+
+
 pm1p0_grimm = (value_buffer[7] << 8 + value_buffer[8])
 pm2p5_grimm = (value_buffer[9] << 8 + value_buffer[10])
 pm10_grimm = (value_buffer[11] << 8 + value_buffer[12])
