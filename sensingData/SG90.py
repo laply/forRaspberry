9f580c77a5ad624c05ad1f5e31bde9bb1bdf8a55
Laply <lapply2635@gmail.com>
2020-04-21 15:10:08 +0900

    update0421

--- sensingData/SG90.py
+++ sensingData/SG90.py
@@ -11,6 +11,13 @@ class SG90:
     def setting(self):
 
         self.beforeUse()
+
+        self.p1 = self.GPIO.PWM(self.pin_motor1, 50)
+        self.p2 = self.GPIO.PWM(self.pin_motor2, 50)
+
+        self.p1.start(0)
+        self.p2.start(0)
+
         self.p1.ChangeDutyCycle(self.loc1)
         self.p2.ChangeDutyCycle(self.loc2)
         self.afterUse()
@@ -20,12 +27,6 @@ class SG90:
         self.GPIO.setup(self.pin_motor1, self.GPIO.OUT)
         self.GPIO.setup(self.pin_motor2, self.GPIO.OUT)
 
-        self.p1 = self.GPIO.PWM(self.pin_motor1, 50)
-        self.p2 = self.GPIO.PWM(self.pin_motor2, 50)
-
-        self.p1.start(0)
-        self.p2.start(0)
-        
     def afterUse(self):
         self.GPIO.setup(self.pin_motor1, self.GPIO.IN)
         self.GPIO.setup(self.pin_motor2, self.GPIO.IN)
