15eb777f5a875a182e13ee0fb3aba5d9fd59e677
Laply <lapply2635@gmail.com>
2020-04-21 17:30:57 +0900

    update0421

--- testcode/sensortest/A/pm2008test1.py
+++ testcode/sensortest/A/pm2008test1.py
@@ -1,10 +1,14 @@
 import smbus
+import time
 
 i2c = smbus.SMBus(1)
 loc = 0x28
 TxBuffer = [0x11, 0x02, 0x0B, 0x07, 0xDB] 
+
 for TxCnt in TxBuffer :
     i2c.write_byte(loc, TxCnt)
+    time.sleep(0.0005)    
+    
 value_buffer = []
 for i in range(0, 32) :
     read = i2c.read_byte(0x28)
