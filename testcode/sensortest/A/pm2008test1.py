94ff351ed4e64f1a2a45baab6811cf62179104a1
Laply <lapply2635@gmail.com>
2020-04-21 18:05:02 +0900

    update0421

--- testcode/sensortest/A/pm2008test1.py
+++ testcode/sensortest/A/pm2008test1.py
@@ -13,12 +13,15 @@ while True:
 
     if value_buffer[1] == 32 and value_buffer[0] == 0x16 :
         print(value_buffer)
-        pm1p0_grimm = (value_buffer[7] << 8 + value_buffer[8])
-        pm2p5_grimm = (value_buffer[9] << 8 + value_buffer[10])
-        pm10_grimm = (value_buffer[11] << 8 + value_buffer[12])
-        pm1p0_tsi = value_buffer[13] << 8 + value_buffer[14]
-        pm2p5_tsi = value_buffer[15] << 8 + value_buffer[16]
-        pm10_tsi = value_buffer[16] << 8 + value_buffer[18]
+        pm1p0_grimm = (value_buffer[7] << 8) + value_buffer[8]
+        pm2p5_grimm = (value_buffer[9] << 8) + value_buffer[10]
+        pm10_grimm = (value_buffer[11] << 8) + value_buffer[12]
+        pm1p0_tsi = (value_buffer[13] << 8) + value_buffer[14]
+        pm2p5_tsi = (value_buffer[15] << 8) + value_buffer[16]
+        pm10_tsi = (value_buffer[16] << 8) + value_buffer[18]
         print("pm1.0_grimm: %f "  % (pm1p0_grimm))
         print("pm25_grimm: %f "  % (pm2p5_grimm))
         print("pm10_grimm: %f "  % (pm10_grimm))
+        print("pm1.0_tsi: %f "  % (pm1p0_tsi))
+        print("pm25_tsi: %f "  % (pm2p5_tsi))
+        print("pm10_tsi: %f "  % (pm10_tsi))
