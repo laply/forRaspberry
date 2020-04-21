59ca24088ee64868a93d981ba0f73f3266e9392f
Laply <lapply2635@gmail.com>
2020-04-21 11:34:20 +0900

    update0421

--- sensingData/sensing.py
+++ sensingData/sensing.py
@@ -45,17 +45,24 @@ class Sensing :
 
 	def setInstance(self, GPIO):
 		if self.useSensor[0] :
-			self.sensorTimerControl.append(DHT11.Control(self.all_pin[0], GPIO, self.topic,[0, 1])) 
+			self.sensorTimerControl.append(DHT11.Control(self.all_pin[0], GPIO, self.topic,[0, 1]))
+			self.sensorTimerControlIndex.append(0)
+			self.sensorTimerControlIndex.append(1)
 		if self.useSensor[1] :
 			self.sensorDetectControl.append(Fire.Control(self.all_pin[1], GPIO, self.topic, 0))
+			self.sensorDetectControlIndex.append(0)
 		if self.useSensor[2] :
-			self.sensorDetectControl.append(Shock.Control(self.all_pin[4], GPIO, self.topic, 1)) 
+			self.sensorDetectControl.append(Shock.Control(self.all_pin[4], GPIO, self.topic, 1))
+			self.sensorDetectControlIndex.append(1)
 		if self.useSensor[3] :
 			self.sensorDetectControl.append(IR.Control(self.all_pin[5], GPIO, self.topic, 2))
+			self.sensorDetectControlIndex.append(2)
 		if self.useSensor[4] :
 			self.sensorDetectControl.append(Gas.Control(self.adc_pin[0], self.topic, 3))
+			self.sensorDetectControlIndex.append(3)
 		if self.useSensor[5] :
 			self.sensorTimerControl.append(Cds.Control(self.all_pin[7], GPIO, self.topic, 2))
+			self.sensorTimerControlIndex.append(2)
 		if self.useSensor[6] :
 			self.button_instance = Button.Control(self.all_pin[6], GPIO, self.topic)
 		if self.useSensor[7] :
@@ -63,7 +70,6 @@ class Sensing :
 			self.led_instance.write(1)
 		if self.useSensor[8] :
 			self.sensorMoveControl.append(SG90.SG90(self.all_pin[8], self.all_pin[9], GPIO))
-
 	def sensingStart(self):
 		self.sensingList()
 		self.reciveControl = self.receive.getData(self.raspid)
