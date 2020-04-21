388313a888745e89a54c20ac46b0887fcf76bb04
Laply <lapply2635@gmail.com>
2020-04-21 16:34:44 +0900

    update0421

--- testcode/sensortest/A/pm2008_test.py
+++ testcode/sensortest/A/pm2008_test.py
@@ -9,3 +9,18 @@ for TxCnt in TxBuffer :
     i2c.write_byte(loc, TxCnt)
 
 print(i2c.read_byte(0x28))
+
+value_buffer = []
+
+for i in range(0, 32) :
+    read = i2c.read_byte(0x28)
+    value_buffer.append(read)
+
+pm1p0_grimm = value_buffer[7] << 8 + value_buffer[8]
+pm2p5_grimm = value_buffer[9] << 8 + value_buffer[10]
+pm10_grimm = value_buffer[11] << 8 + value_buffer[12]
+pm1p0_tsi = value_buffer[13] << 8 + value_buffer[14]
+pm2p5_tsi = value_buffer[15] << 8 + value_buffer[16]
+pm10_tsi = value_buffer[16] << 8 + value_buffer[18]
+
+print("pm1p0_grimm: " + pm1p0_grimm)
