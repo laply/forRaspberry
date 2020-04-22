753248e797da66cabf339e4a2ad5908e2b730c0e
Laply <lapply2635@gmail.com>
2020-04-22 13:13:56 +0900

    update

--- sensingData/Connect.py
+++ sensingData/Connect.py
@@ -7,8 +7,11 @@ class Connect :
 
     def __init__(self, ipPort):
         print("MQTT-init")
-        self.client.connect(ipPort[0], ipPort[1])
+        self.ipPort = ipPort
         print("MQTT-connect")
+
+    def start(self):
+        self.client.connect(self.ipPort[0], self.ipPort[1])
         try:
         	self.client.loop_start()
         except KeyboardInterrupt:
