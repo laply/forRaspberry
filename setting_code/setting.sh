9b535215450c2027bbc6b38f3bf6eeb2a11b8e09
Laply <lapply2635@gmail.com>
2020-05-06 10:17:50 +0900

    update0506 setself update

--- setting_code/setting.sh
+++ setting_code/setting.sh
@@ -1,34 +1,37 @@
 #!bin/bash
 
 if [ ${1} -eq "0" ]; then
-echo "[start Tcs sensing setting at pi]"
-echo "[network setting]"
-sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
-sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+    echo "[start Tcs sensing setting at pi]"
+    echo "[network setting]"
+    sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
+    sudo cp ./setting_file/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
-sed -e "19 i/sudo sh ${pwd}/setting.sh 1 &" /etc/rc.local
+    sudo rm /etc/rc.local
+    sudo cp ./setting_file/rc_1.local /etc/rc.local
 
+    sudo reboot
 elif [ ${1} -eq "1" ]; then
-echo "[init setting install]"
-sudo apt-get update
-sudo apt-get upgrade
+    echo "[init setting install]"
+    sudo apt-get update
+    sudo apt-get upgrade
 
-sudo apt-get install git 
-sudp apt-get install python-pip
+    sudo apt-get install git 
+    sudp apt-get install python-pip
 
-git clone https://github.com/munu2635/raspstart.git
+    git clone https://github.com/munu2635/raspstart.git
 
-sudo apt-get install cmake libjpeg-dev
-cd raspstart/mjpg-streamer/mjpg-streamer-experimental/
-make CMAKE_BUILD_TYPE=Debug
-cd ..
+    sudo apt-get install cmake libjpeg-dev
+    cd raspstart/mjpg-streamer/mjpg-streamer-experimental/
+    make CMAKE_BUILD_TYPE=Debug
+    cd ..
 
-sudo install spidev
-sudo apt-get install python-smbus
-sed -e "19 i/sudo sh home/pi/raspstart/mjpg.sh &" /etc/rc.local
-sed -e "20 i/sudo sh sudo -H -u pi usr/bin/python home/pi/raspstart/sensingData/sensingMain.py &" /etc/rc.local
+    sudo install spidev
+    sudo apt-get install python-smbus
 
-echo "[End Tcs sensing setting at pi]"
+    sudo rm /etc/rc.local
+    sudo cp ./setting_file/rc_2.local /etc/rc.local
+
+    echo "[End Tcs sensing setting at pi]"
 
 fi
 
